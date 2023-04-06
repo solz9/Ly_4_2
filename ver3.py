313ec85cba0ef9d453943e4fa19e7a56fa665375
solz9 <125681221+solz9@users.noreply.github.com>
2023-04-06 19:27:28 +0700

    Update ver3.py

--- ver3.py
+++ ver3.py
@@ -4,10 +4,10 @@ import pandas as pd
 import numpy as np
 st.title('KẾT QUẢ HỌC TẬP LỚP LÝ 4')
 option = st.selectbox('Bạn là?', ('Học sinh', 'Giáo viên'))
-x = st.number_input('Nhập Password')
 df = pd.read_excel('DS_10Ly4 - Copy.xlsx')
 dfs = pd.read_excel('passgv.xlsx')
 if option == 'Giáo viên':
+    x = st.number_input('Nhập Password')
     if st.button('Enter'):
         if dfs['Password'].values != x:
             st.warning('Bạn đã nhập sai Password hoặc họ và tên, vui lòng nhập lại')
@@ -57,6 +57,7 @@ if option == 'Giáo viên':
 #             st.table(df2)
 else:
     y = st.text_input('Nhập họ và tên (Lưu ý: ghi hoa chữ cái đầu)')
+    x = st.number_input('Nhập Password')
     df1 = df[df['Họ và tên'] == y]
     if df1['Password'].values != x:
         st.warning('Bạn đã nhập sai Password hoặc họ và tên, vui lòng nhập lại')
