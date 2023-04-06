02f69df035c5fbaed7eef056313cf7d043be2c79
solz9 <125681221+solz9@users.noreply.github.com>
2023-04-06 19:39:28 +0700

    Update ver3.py

--- ver3.py
+++ ver3.py
@@ -59,29 +59,30 @@ else:
     y = st.text_input('Nhập họ và tên (Lưu ý: ghi hoa chữ cái đầu)')
     x = st.text_input("Nhập mật khẩu", type="password")
     df1 = df[df['Họ và tên'] == y]
-    if df1['Password'].values != x:
-        st.warning('Bạn đã nhập sai Password hoặc họ và tên, vui lòng nhập lại')
-    else:
-        df1 = pd.DataFrame(
-            {
-                "Họ và tên": df1['Họ và tên'],
-                "HS1": df1['HS1'],
-                'BT01 Đúng/Sai': df1['BT01 Đúng/Sai'],
-                'BT02 Moment': df1['BT02 Moment'],
-                'Điểm cộng': df1['Điểm cộng'],
-                'Điểm KTGK': df1['Điểm KTGK'],
-                'Điểm KTCK': df1['Điểm KTCK'],
-            }
-        )
-        hide_dataframe_row_index = """
-        <style>
-        .row_heading.level0 {display:none}
-        .blank {display:none}
-        </style>
-        """
+    if st.button('Enter'):
+        if df1['Password'].values != x:
+            st.warning('Bạn đã nhập sai Password hoặc họ và tên, vui lòng nhập lại')
+        else:
+            df1 = pd.DataFrame(
+                {
+                    "Họ và tên": df1['Họ và tên'],
+                    "HS1": df1['HS1'],
+                    'BT01 Đúng/Sai': df1['BT01 Đúng/Sai'],
+                    'BT02 Moment': df1['BT02 Moment'],
+                    'Điểm cộng': df1['Điểm cộng'],
+                    'Điểm KTGK': df1['Điểm KTGK'],
+                    'Điểm KTCK': df1['Điểm KTCK'],
+                }
+            )
+            hide_dataframe_row_index = """
+            <style>
+            .row_heading.level0 {display:none}
+            .blank {display:none}
+            </style>
+            """
 
-                    # Inject CSS with Markdown
-        st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
+                        # Inject CSS with Markdown
+            st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
 
-                    # Display an interactive table
-        st.table(df1)
+                        # Display an interactive table
+            st.table(df1)
