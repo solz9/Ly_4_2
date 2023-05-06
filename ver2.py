7f91df2e47a63959f3508b724b0f88c48bf6c952
solz9 <solomon131006@gmail.com>
2023-05-06 21:40:15 +0700

    Update ver2.py

--- ver2.py
+++ ver2.py
@@ -11,27 +11,49 @@ if st.button('Enter'):
     if df1['Password'].values != x:
         st.warning('Bạn đã nhập sai Password hoặc họ và tên, vui lòng nhập lại')
     else:
-        df1 = pd.DataFrame(
-            {
-                "Họ và tên": df1['Họ và tên'],
-                "HS1": df1['HS1'],
-                'BT01 Đúng/Sai': df1['BT01 Đúng/Sai'],
-                'BT02 Moment': df1['BT02 Moment'],
-                'Điểm cộng': df1['Điểm cộng'],
-                'Điểm KTGK': df1['Điểm KTGK'],
-                'Điểm KTCK': df1['Điểm KTCK'],
-                'Điểm KTTX': df1['KTTX']
-            }
-        )
-        hide_dataframe_row_index = """
-        <style>
-        .row_heading.level0 {display:none}
-        .blank {display:none}
-        </style>
-        """
+        if df[df['Họ và tên'] == Nguyễn Lục Hoàng Minh]:
+            df2 = pd.DataFrame(
+                {
+                    "Họ và tên": df['Họ và tên'],
+                    "HS1": df['HS1'],
+                    'BT01 Đúng/Sai': df['BT01 Đúng/Sai'],
+                    'BT02 Moment': df['BT02 Moment'],
+                    'Điểm cộng': df['Điểm cộng'],
+                    'Điểm KTGK': df['Điểm KTGK'],
+                    'Điểm KTCK': df['Điểm KTCK'],
+                    'Điểm KTTX': df['KTTX']
+                }
+            )
+            hide_dataframe_row_index = """
+            <style>
+            .row_heading.level0 {display:none}
+            .blank {display:none}
+            </style>
+            """
+            st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
+            st.table(df2)
+        else:
+            df1 = pd.DataFrame(
+                {
+                    "Họ và tên": df1['Họ và tên'],
+                    "HS1": df1['HS1'],
+                    'BT01 Đúng/Sai': df1['BT01 Đúng/Sai'],
+                    'BT02 Moment': df1['BT02 Moment'],
+                    'Điểm cộng': df1['Điểm cộng'],
+                    'Điểm KTGK': df1['Điểm KTGK'],
+                    'Điểm KTCK': df1['Điểm KTCK'],
+                    'Điểm KTTX': df1['KTTX']
+                }
+            )
+            hide_dataframe_row_index = """
+            <style>
+            .row_heading.level0 {display:none}
+            .blank {display:none}
+            </style>
+            """
 
-        # Inject CSS with Markdown
-        st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
+            # Inject CSS with Markdown
+            st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
 
-        # Display an interactive table
-        st.table(df1)
+            # Display an interactive table
+            st.table(df1)
